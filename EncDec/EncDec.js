6aac0e127c382df7895118443bb1cdde35d70eea
TakeAsh <take@TakeAsh.net>
2022-11-22 05:33:41 +0900

    add EncDec

---
+++ EncDec/EncDec.js
@@ -0,0 +1,29 @@
+﻿const d = document;
+const txtSrc = d.getElementById('txtSrc');
+const txtDst = d.getElementById('txtDst');
+
+function EncURI() {
+  txtDst.value = encodeURI(txtSrc.value);
+}
+
+function DecURI() {
+  txtDst.value = txtSrc.value.replace(
+    /((%[8-9A-Fa-f][0-9A-Fa-f])*%[0-9A-Fa-f]{2})/g,
+    (whole, p1) => decodeURI(p1)
+  );
+}
+
+function EncURIC() {
+  txtDst.value = encodeURIComponent(txtSrc.value);
+}
+
+function DecURIC() {
+  txtDst.value = txtSrc.value.replace(
+    /((%[8-9A-Fa-f][0-9A-Fa-f])*%[0-9A-Fa-f]{2})/g,
+    (whole, p1) => decodeURIComponent(p1)
+  );
+}
+
+function Cpy() {
+  txtSrc.value = txtDst.value;
+}
