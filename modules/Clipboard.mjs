b638c4c174927c6b04998762e0631c1de1e35f08
TakeAsh <take@TakeAsh.net>
2022-12-26 00:53:05 +0900

    add Clipboard.mjs

---
+++ modules/Clipboard.mjs
@@ -0,0 +1,46 @@
+﻿const d = document;
+
+/**
+ * Copy text to clipboard.
+ * @param {string} text
+ * @return {boolean} true:success false:failure
+ */
+function copyToClipboard(text) {
+  if (!d.execCommand) { return false; } /* execCommand is deprecated */
+  const textarea = d.createElement('textarea');
+  textarea.textContent = text;
+  d.body.appendChild(textarea);
+  textarea.select();
+  d.execCommand('copy');
+  d.body.removeChild(textarea);
+  return true;
+}
+
+/**
+ * Write text to clipboard.
+ * @param {string} text
+ * @return {boolean} true:success false:failure
+ */
+function writeToClipboard(text) {
+  if (!navigator.clipboard.writeText) { return false; }
+  return navigator.permissions.query({ name: 'clipboard-write' })
+    .then((result) => {
+      if (result.state === 'granted' || result.state === 'prompt') {
+        navigator.clipboard.writeText(text)
+          .then(
+            () => { return true; },
+            (err) => {
+              console.log(err);
+              alert('Document is not focused.\nClick document and try again.');
+              return false;
+            }
+          );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+}
+
+export { copyToClipboard, writeToClipboard };
