e79e7af7b9d8ece91e89f5ad31d6b1557c57a797
TakeAsh <take@TakeAsh.net>
2023-01-13 04:52:54 +0900

    add Cookie.mjs

---
+++ modules/Cookie.mjs
@@ -0,0 +1,19 @@
+﻿const d = document;
+
+function getCookie(key) {
+  const m = d.cookie.match(new RegExp(key + '\\s*=\\s*([^;]+)'));
+  return !m || !m[1]
+    ? null
+    : JSON.parse(m[1]);
+}
+
+function setCookie(key, obj, expireDate) {
+  let cookieStr = key + '=' + JSON.stringify(obj) + ';path=/';
+  if (expireDate > 0) {
+    expireDate = new Date(new Date().getTime() + 60 * 60 * 24 * expireDate * 1000).toGMTString();
+    cookieStr += ';expires=' + expireDate;
+  }
+  d.cookie = cookieStr;
+}
+
+export { getCookie, setCookie };
