7d4df62dab9963b52832197dc55b7dbbcf1dd3bd
TakeAsh <take@TakeAsh.net>
2023-03-20 03:12:19 +0900

    add Clock

---
+++ Clock/clock.js
@@ -0,0 +1,21 @@
+﻿const d = document;
+const clock = d.getElementById('clock');
+let prevSecond = null;
+setInterval(showTime, 100);
+
+function showTime(event) {
+  const now = new Date();
+  const [hour, minute, second] = [
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds()
+  ];
+  if (prevSecond == second) { return; }
+  prevSecond = second;
+  const [txtHour, txtMinute, txtSecond] = [
+    String(hour).padStart(2, '0'),
+    String(minute).padStart(2, '0'),
+    String(second).padStart(2, '0'),
+  ];
+  clock.textContent = `${txtHour}:${txtMinute}:${txtSecond}`;
+}
