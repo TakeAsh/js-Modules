63120fb8b92c01027c1fb5f9b747e3d35e00e766
TakeAsh <take@TakeAsh.net>
2023-06-11 10:25:20 +0900

    Cookie: tune code

--- modules/Cookie.mjs
+++ modules/Cookie.mjs
@@ -1,19 +1,19 @@
 ﻿const d = document;
 
 function getCookie(key) {
-  const m = d.cookie.match(new RegExp(key + '\\s*=\\s*([^;]+)'));
+  const m = d.cookie.match(new RegExp(`${key}\\s*=\\s*([^;]+)`));
   return !m || !m[1]
     ? null
     : JSON.parse(m[1]);
 }
 
 function setCookie(key, obj, expireDate) {
-  let cookieStr = key + '=' + JSON.stringify(obj) + ';path=/';
+  const cookie = [`${key}=${JSON.stringify(obj)}`, 'path=/'];
   if (expireDate > 0) {
     expireDate = new Date(new Date().getTime() + 60 * 60 * 24 * expireDate * 1000).toGMTString();
-    cookieStr += ';expires=' + expireDate;
+    cookie.push(`expires=${expireDate}`);
   }
-  d.cookie = cookieStr;
+  d.cookie = cookie.join(';');
 }
 
 export { getCookie, setCookie };
