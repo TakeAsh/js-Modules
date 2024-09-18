1878c136c190e3d261c70cae19348587ae7cae64
TakeAsh <take@TakeAsh.net>
2024-09-18 21:26:22 +0900

    add CyclicEnum module

---
+++ modules/CyclicEnum.mjs
@@ -0,0 +1,24 @@
+﻿class CyclicEnum extends Array {
+  constructor(...args) {
+    super();
+    args.forEach((key, index) => Object.defineProperty(this, key, {
+      value: this[index] = Object.freeze({
+        toString: () => key,
+        index: index,
+        next: () => this[(index + 1) % args.length],
+      }),
+      enumerable: false,
+    }));
+    Object.freeze(this);
+  }
+
+  static get [Symbol.species]() { return Array; }
+
+  get(key) {
+    return this.hasOwnProperty(key) ?
+      this[key] :
+      this[0];
+  }
+}
+
+export { CyclicEnum };
