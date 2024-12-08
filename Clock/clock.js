75e555ac91730c1efe8c6aa82e163bb43b67098a
TakeAsh <take@TakeAsh.net>
2024-12-08 15:53:37 +0900

    Clock: sample of AutoSaveConfig that has an instance of ClockColors class

--- Clock/clock.js
+++ Clock/clock.js
@@ -1,21 +1,36 @@
 ﻿import { AutoSaveConfig } from '../modules/AutoSaveConfig.mjs';
+import { ClockColor, ClockColors } from './modules/ClockColor.mjs';
 
 const d = document;
+const systemColor = 'System;#ffffff;#000000';
 const config = new AutoSaveConfig(
-  { ColorMode: 'System', },
+  {
+    ClockColors: new ClockColors(
+      systemColor,
+      'Light;#000000;#d0d0f0', 'Dark;#e4e4e4;#2f2f0f',
+      'Blue;#e4e4e4;#2020ff', 'Green;#00e000;#2f2f0f',
+      'Amber;#ffbf00;#2f2f0f',
+    ),
+    ClockColor: systemColor,
+  },
   'Clock'
 );
 const clock = d.getElementById('clock');
+const selectColor = d.getElementById('selectColor');
+const inputName = d.getElementById('inputName');
+const inputText = d.getElementById('inputText');
+const inputBackground = d.getElementById('inputBackground');
+d.getElementById('buttonAddColor').addEventListener('click', addColor);
+d.getElementById('buttonRemoveColor').addEventListener('click', removeColor);
 let prevSecond = null;
 setInterval(showTime, 100);
-const selectColorMode = d.getElementById('selectColorMode');
-const colorModes = Array.from(selectColorMode.options)
-  .map(option => option.value);
-selectColorMode.addEventListener(
+config.ClockColors.toOptions()
+  .forEach(option => selectColor.appendChild(option));
+selectColor.addEventListener(
   'change',
-  (event) => { setColorMode(selectColorMode.value); }
+  (event) => { setColor(selectColor.value); }
 );
-setColorMode(config.ColorMode);
+setColor(config.ClockColor);
 
 function showTime(event) {
   const now = new Date();
@@ -34,13 +49,41 @@ function showTime(event) {
   clock.textContent = `${txtHour}:${txtMinute}:${txtSecond}`;
 }
 
-function setColorMode(color) {
-  color = color || 'System';
-  config.ColorMode = selectColorMode.value = color;
-  colorModes.forEach(color2 => { d.body.classList.remove(`${color2}Mode`); });
-  if (color == 'System') {
+function setColor(color) {
+  color = color || systemColor;
+  const clockColor = new ClockColor(config.ClockColor = selectColor.value = color);
+  d.body.style.color = null;
+  d.body.style.backgroundColor = null;
+  if (clockColor.name == 'System') {
     // use System Light/Dark mode
   } else {
-    d.body.classList.add(`${color}Mode`);
+    d.body.style.color = clockColor.textColor;
+    d.body.style.backgroundColor = clockColor.backgroundColor;
+  }
+  inputName.value = clockColor.name;
+  inputText.value = clockColor.textColor;
+  inputBackground.value = clockColor.backgroundColor;
+}
+
+function addColor(ev) {
+  const color = new ClockColor(inputName.value, inputText.value, inputBackground.value);
+  removeOption(color);
+  selectColor.appendChild(color.toOption());
+  config.ClockColors.push(color);
+  config.ClockColor = selectColor.value = color.toJSON();
+  setColor(color.toJSON());
+}
+
+function removeColor(ev) {
+  const color = new ClockColor(inputName.value, inputText.value, inputBackground.value);
+  removeOption(color);
+  config.ClockColors.remove(color);
+  setColor(systemColor);
+}
+
+function removeOption(color) {
+  const option = selectColor.querySelector(`option[value^="${color.name};"]`);
+  if (option) {
+    selectColor.removeChild(option);
   }
 }
---
+++ Clock/modules/ClockColor.mjs
@@ -0,0 +1,71 @@
+﻿import { prepareElement } from '../../modules/PrepareElement.mjs';
+
+class ClockColor {
+  constructor() {
+    this.name = 'W/B';
+    this.textColor = '#ffffff';
+    this.backgroundColor = '#000000';
+    this.assign(...arguments);
+  }
+  toJSON() { return `${this.name};${this.textColor};${this.backgroundColor}`; }
+  assign(src) {
+    let m;
+    if (arguments.length == 1
+      && typeof arguments[0] == 'string'
+      && (m = /^(?<name>[^;]+);(?<text>#[0-9A-Fa-f]{6});(?<back>#[0-9A-Fa-f]{6})$/.exec(arguments[0]))) {
+      this.name = m.groups['name'].replace(/;/, '_');
+      this.textColor = m.groups['text'];
+      this.backgroundColor = m.groups['back'];
+    } else if (arguments.length == 3) {
+      this.name = arguments[0].replace(/;/, '_');
+      this.textColor = arguments[1];
+      this.backgroundColor = arguments[2];
+    }
+  }
+  valueOf() { return this.toJSON(); }
+  compareTo(other) { return this.name.localeCompare(other.name); }
+  toOption() {
+    return prepareElement({ tag: 'option', text: this.name, value: this.toJSON(), });
+  }
+}
+
+class ClockColors extends Array {
+  static get [Symbol.species]() { return Array; }
+  constructor() {
+    super();
+    this.names = [];
+    this.assign(Array.from(arguments));
+  }
+  toJSON() { return this.map(color => color.toJSON()); }
+  assign(src) {
+    if (!Array.isArray(src) || src.length == 0) { return; }
+    this.push(...src.map(color => new ClockColor(color)));
+  }
+  push() {
+    Array.from(arguments)
+      .filter(color => color instanceof ClockColor)
+      .forEach(color => {
+        if (this.includes(color)) { this.remove(color); }
+        super.push(color);
+        //this.sort((a, b) => a.compareTo(b));
+        this.names = this.map(color => color.name);
+      });
+    return this;
+  }
+  includes(color) {
+    return color instanceof ClockColor
+      && this.names.includes(color.name);
+  }
+  remove(color) {
+    const index = this.names.indexOf(color.name);
+    if (index < 0) { return this; }
+    this.splice(index, 1);
+    this.names = this.map(color => color.name);
+    return this;
+  }
+  toOptions() {
+    return this.map(color => color.toOption());
+  }
+}
+
+export { ClockColor, ClockColors };
