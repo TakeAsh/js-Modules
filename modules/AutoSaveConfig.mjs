cec2f2e5eaa9727de9180bb98bdc0de543b16352
TakeAsh <take@TakeAsh.net>
2024-12-08 15:51:45 +0900

    AutoSaveConfig: allow instance of some class as a property - The instance must be rebuild by "assign" method with its "toJSON" outputed object.

--- modules/AutoSaveConfig.mjs
+++ modules/AutoSaveConfig.mjs
@@ -8,12 +8,14 @@ class AutoSaveConfig {
   /**
    * Creates an instance of AutoSaveConfig.
    * This class saves the properties in localStorage automatically when they are set.
+   * If a property of configDefault is an object and it has 'assign' method, 
+   * the property is not overwritten but AutoSaveConfig calls 'assign' method with saved object.
    * @param {*} [configDefault={}] - default values of config
    * @param {string} [name='Config'] - name in localStorage
    * @memberof AutoSaveConfig
    */
   constructor(configDefault = {}, name = 'Config') {
-    this.config = JSON.parse(JSON.stringify(configDefault)); // clone default
+    this.config = configDefault;
     this.nameInStorage = name;
     this.handler = {
       set: (obj, prop, value) => {
@@ -23,7 +25,7 @@ class AutoSaveConfig {
       },
     };
     const savedConfig = JSON.parse(localStorage.getItem(this.nameInStorage)) || {};
-    Object.assign(this.config, savedConfig);
+    AutoSaveConfig.assign(this.config, savedConfig);
     return this.toProxy(this.config);
   }
 
@@ -31,6 +33,16 @@ class AutoSaveConfig {
     if (value === null || typeof value !== 'object' || Object.isFrozen(value)) { return value; }
     Object.keys(value).forEach((p) => { value[p] = this.toProxy(value[p]); });
     return new Proxy(value, this.handler);
+  }
+
+  static assign(dest, src) {
+    for (const prop in dest) {
+      if (dest[prop] !== null && typeof dest[prop].assign == 'function') {
+        dest[prop].assign(src[prop]);
+      } else {
+        dest[prop] = src[prop];
+      }
+    }
   }
 }
 
